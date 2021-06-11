ba258f37f409d78fe9dd7f8ea911137deb279259
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-06-11 10:08:06 -0300

    Files Upload

---
+++ Web Scraping/9-Waits.py
@@ -0,0 +1,28 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from functools import partial
+
+
+f = Firefox()
+# f.implicitly_wait(15)
+
+wdw = WebDriverWait(f, 30, 2)
+
+
+def wait(element, by, webdriver):
+    print(f'Tentando encontrar "{element}" by {by}')
+    if webdriver.find_elements(by, element):
+        return True
+    return False
+
+
+f.get('https://selenium.dunossauro.live/aula_09_a.html')
+wdw.until(partial(wait, 'button', By.CSS_SELECTOR), 'this program sucks')
+
+f.find_element_by_css_selector('button').click()
+
+wdw.until(partial(wait, '#finished', By.CSS_SELECTOR))
+
+fns = f.find_element_by_css_selector('#finished')
+assert fns.text == 'Carregamento concluído'
