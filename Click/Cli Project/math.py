5f805a4a6eb00c093d9ce78aa66c41f16cf3373d
Usuario <kevinho_gameplays@hotmail.com>
2021-06-16 09:52:43 -0300

    files upload

--- Click/Cli Project/math.py
+++ Click/Cli Project/math.py
@@ -7,9 +7,22 @@ def math():
     ...
 
 
-@math.command()
-@click.option('-s', type=click.INT, Default=0, help='Randomization start number')
-@click.option('-e', type=click.INT, Default=9, help='Randomization end number')
-@click.option('-c', type=click.INT, Default=5, help='Number of characters in random output')
-def random(start, end, characters):
+@math.command(help='Randomize numbers')
+@click.option('-s', type=click.INT, default=0, help='Randomization start number')
+@click.option('-e', type=click.INT, default=9, help='Randomization end number')
+@click.option('-c', type=click.INT, default=5, help='Number of characters in random output')
+def random(s=0, e=9, c=5):
+    if s != 0:
+        e = s + 9
+    randstr = ''
+    for r in range(0, c):
+        randstr += str((randint(s, e)))
+    if len(randstr) > c:
+        click.echo(randstr[0:c+1])
+    else:
+        click.echo(randstr)
 
+
+
+
+math()
