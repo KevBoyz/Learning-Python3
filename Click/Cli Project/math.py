2839aeb5dbfc3a3d982996592c2eb68e6fb714c4
Usuario <kevinho_gameplays@hotmail.com>
2021-06-16 11:06:07 -0300

    files upload

--- Click/Cli Project/math.py
+++ Click/Cli Project/math.py
@@ -18,11 +18,26 @@ def random(s=0, e=9, c=5):
     for r in range(0, c):
         randstr += str((randint(s, e)))
     if len(randstr) > c:
-        click.echo(randstr[0:c+1])
+        click.echo(randstr[0:c + 1])
     else:
         click.echo(randstr)
 
 
+@math.command(help='Calculate simple probability')
+@click.option('-pc', type=click.INT, prompt=True, help='Possible cases')
+@click.option('-fc', type=click.INT, prompt=True, help='Favorable cases')
+@click.option('-d', '-decimal', is_flag=True, default=False, help='[FLAG] Decimal output')
+def prob(pc, fc, d):
+    if fc > pc:
+        try:
+            raise Exception('[fc > pc], Operation not possible')
+        except Exception as e:
+            click.echo(e)
+            return
+    if d:
+        click.echo(f'{fc / pc}')
+    else:
+        click.echo(f'{int((fc / pc) * 100)}%')
 
 
 math()
