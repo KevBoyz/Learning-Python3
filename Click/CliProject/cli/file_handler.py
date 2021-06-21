38d5391fc5dffb49fc779fa0f594260d8b889cdc
Usuario <kevinho_gameplays@hotmail.com>
2021-06-21 10:27:34 -0300

    bug fix

--- Click/CliProject/cli/file_handler.py
+++ Click/CliProject/cli/file_handler.py
@@ -12,23 +12,36 @@ def Zip():
 @Zip.command(help='Compress all path FILES in zip')
 @click.argument('path', type=click.Path(exists=True))
 @click.option('-fn', type=click.STRING, default='ZipFile', help='Zip file name')
-def compress(path, fn):
+@click.option('-v', '--verbose', is_flag=True, default=False, show_default=True, help='Verbose mode')
+def compress(path, fn, v):
     os.chdir(path)
     cwd = os.getcwd()
     files = os.listdir()
     if len(files) <= 0:
+        print(f'Files founded: {files}') if v else None     # Verbose
         raise Exception('Error, no files or directories founded in this path')
-    zip = zipfile.ZipFile(fn + '.zip', 'w')
+    if not fn.rfind('.zip'):
+        fn += '.zip'
+    zip = zipfile.ZipFile(fn, 'w')
+    files.remove(fn)
     with click.progressbar(range(len(files))) as p:
         for c in p:
-            if files[c] != fn + '.zip' and files[c] not in zip.namelist() and not os.path.isdir(files[c]):
+            if not os.path.isdir(files[c]):
                 zip.write(files[c], compress_type=zipfile.ZIP_DEFLATED)
+                print('Compressing:', files[c]) if v else None  # Verbose
+                files.remove(files[c])
             if os.path.isdir(files[c]):
                 os.chdir(files[c])
                 try:
                     for file in os.listdir():
                         if not os.path.isdir(file) and file not in zip.namelist():
                             zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+                        elif os.path.isdir(file):
+                            os.chdir(file)
+                            for file in os.listdir():
+                                if not os.path.isdir(file) and file not in zip.namelist():
+                                    zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+                            os.chdir('..')
                 except:
                     continue
                 os.chdir(cwd)
