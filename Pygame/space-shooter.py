660fb1a31d019c132715fc062a404f6e6b511f4d
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-08-25 17:21:26 -0300

    Game update

--- Pygame/space-shooter.py
+++ Pygame/space-shooter.py
@@ -2,10 +2,11 @@ import pygame
 from pygame import display
 from pygame.image import load
 from pygame.transform import scale
-from pygame.sprite import Sprite, GroupSingle, Group
+from pygame.sprite import Sprite, GroupSingle, Group, groupcollide
 from pygame import event
 from pygame.locals import QUIT, KEYDOWN, K_SPACE, K_a, K_d
 from pygame.time import Clock
+from random import randint
 
 pygame.init()
 
@@ -15,16 +16,27 @@ class SpaceShip(Sprite):
         super().__init__()
         self.image = load('Sprites/spaceship.png')  # Sprite
         self.rect = self.image.get_rect(center=(540, 550))  # Hit box
-        self.speed = 3
+        self.speed = 3.5
         self.bullet = bullet
+        self.life = 3
 
     def shot(self):
         self.bullet.add(Bullet(self.rect.x + 105, self.rect.y))
         self.bullet.add(Bullet(self.rect.x + 45, self.rect.y + 90))
         self.bullet.add(Bullet(self.rect.x + 160, self.rect.y + 90))
-        self.bullet.update()
+
+    def hit(self):
+        global round
+        if self.life <= 0:
+            self.kill()
+            pygame.quit()
+        else:
+            self.life -= 1
 
     def update(self):
+        global laser, nave
+        if groupcollide(laser, nave, True, False):
+            self.hit()
         keys = pygame.key.get_pressed()
         if keys[K_a]:
             if not self.rect.x <= 0:
@@ -48,29 +60,61 @@ class Bullet(Sprite):
 
 
 class OVNI(Sprite):
-    def __init__(self):
+    def __init__(self, laser, x, y):
         super().__init__()
         self.image = load('Sprites/ovni.png')
-        self.rect = self.image.get_rect(center=(540, 50))
+        self.rect = self.image.get_rect(center=(x, y))
+        self.speed = 4
         self.direction = 0
+        self.laser = laser
+        self.life = 3
+
+    def shot(self):
+        self.laser.add(Laser(self.rect.x + 81, self.rect.y + 15))
+        self.laser.add(Laser(self.rect.x + 181, self.rect.y + 15))
+
+    def hit(self):
+        if self.life <= 0:
+            self.kill()
+        else:
+            self.life -= 1
 
     def update(self):
+        global bullet, ufo
+        if groupcollide(bullet, ufo, True, False):
+            self.hit()
+        if randint(0, 200) == 18:
+            self.shot()
         if self.direction == 0:
-            self.rect.x -= 2
+            self.rect.x -= self.speed
             if self.rect.x <= 0:
                 self.direction = 1
         else:
-            self.rect.x += 2
+            self.rect.x += self.speed
             if self.rect.x >= 850:
                 self.direction = 0
 
+
+class Laser(Sprite):
+    def __init__(self, x, y):
+        super(Laser, self).__init__()
+        self.image = load('Sprites/ufo-laser.png')
+        self.rect = self.image.get_rect(center=(x, y))
+        self.speed = 3
+
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y >= 700:
+            self.kill()
+
+
 bullet = Group()
+laser = Group()
 
 spship = SpaceShip(bullet)
 nave = GroupSingle(spship)
 
-ovni = OVNI()
-ufo = GroupSingle(ovni)
+ufo = Group()
 
 size = (1080, 700)
 window = display.set_mode(
@@ -85,20 +129,26 @@ display.set_caption('Nego Dí Rapariga NÃO - Brega version')
 bg = scale(load('images/space-bg.jpg'), size)
 
 clock = Clock()
+deaths = 0
+round = 0
 while True:
-    clock.tick(300)  # Frames per second
+    clock.tick(300)  # FPS
     for ev in event.get():  # Close event
         if ev.type == QUIT:
             pygame.quit()
         if ev.type == KEYDOWN:
             if ev.key == K_SPACE:
                 spship.shot()
-
     window.blit(bg, (0, 0))  # (200, 50) Position, 4 cartesian quadrant
     nave.draw(window)
+    if round % 200 == 0 and len(ufo) <= 10:
+        ufo.add(OVNI(laser, randint(0, 1080), randint(0, 200)))
     ufo.draw(window)
     bullet.draw(window)
+    laser.draw(window)
     nave.update()
     bullet.update()
+    laser.update()
     ufo.update()
     display.update()
+    round += 1
