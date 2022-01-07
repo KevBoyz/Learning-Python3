7240c4a0273637315fea7f127095209b6de2ab29
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-01-07 15:02:28 -0300

    update

--- Kivy/6KivyMd.py
+++ Kivy/6KivyMd.py
@@ -1,5 +1,8 @@
 from kivymd.app import MDApp
 from kivymd.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDRaisedButton
 from os import listdir
 import shutil as sh
 
@@ -11,8 +14,30 @@ class MyScreen(FloatLayout):
     ...
 
 
+class FormLogin(FloatLayout):
+    dialog = None
+    def open_card(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Password Retriever",
+                type="custom",
+                content_cls=Content(),
+
+            )
+        self.dialog.open()
+
+
+
+
+class Content(BoxLayout):
+    pass
+
+
 class Body6App(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = 'Green'
+        self.theme_cls.accent_palette = 'Purple'
+        self.theme_cls.theme_style = 'Dark'
         return MyScreen()
 
 
