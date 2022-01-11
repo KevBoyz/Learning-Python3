59007a1be5b66976de46b4aea28eb42791ee82a0
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-01-11 12:18:04 -0300

    update

--- Built-in/sockets/client.py
+++ Built-in/sockets/client.py
@@ -3,10 +3,19 @@ import socket
 host = socket.gethostbyname(socket.gethostname())
 port = 7562
 
+
+# s.connect_ex  # Connect without stop te application
+
+"""
+import socket
+
+host = socket.gethostbyname(socket.gethostname())
+port = 7562
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))  # Request the server to connect
     s.sendall(b'Ola!')
     data = s.recv(1024)
 
 print('Recebido: ', repr(data))
-
+"""
