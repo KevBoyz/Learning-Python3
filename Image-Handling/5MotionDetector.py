2086797c1c0a9841881e4c91f46dbf6b18ee26d8
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-01-18 22:19:20 -0300

    update

--- Image-Handling/5MotionDetector.py
+++ Image-Handling/5MotionDetector.py
@@ -3,10 +3,13 @@ import cv2
 cap = cv2.VideoCapture(0)
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+out = cv2.VideoWriter("output.avi", fourcc, 15.0, (1366, 768))
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 while cap.isOpened():
+    image = cv2.resize(frame1, (1366, 768))
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -14,7 +17,7 @@ while cap.isOpened():
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) > 0:
-        print('Movement')
+        out.write(image)
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
         if cv2.contourArea(contour) < 900:
@@ -22,7 +25,7 @@ while cap.isOpened():
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                     1, (0, 0, 255), 3)
-    image = cv2.resize(frame1, (1280, 720))
+    image = cv2.resize(frame1, (1366, 768))
     cv2.imshow("feed", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
@@ -31,4 +34,4 @@ while cap.isOpened():
 
 cv2.destroyAllWindows()
 cap.release()
-
+out.release()
