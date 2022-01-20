ad6d1209ace03ba8afb95b1d180129fc38013dfd
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-01-20 20:19:51 -0300

    update

--- Kivy/10KvOpencv.py
+++ Kivy/10KvOpencv.py
@@ -12,7 +12,7 @@ class MainApp(MDApp):
         layout = MDBoxLayout(orientation='vertical')
         self.image = Image()
         layout.add_widget(self.image)
-        self.capture = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0)
         layout.add_widget(MDRaisedButton(
             text='Click me',
             pos_hint={'center_x': .5, 'center_y': .5},
@@ -22,9 +22,22 @@ class MainApp(MDApp):
         return layout
 
     def load_video(self, *args):
-        ret, frame = self.capture.read()
-        buffer = cv2.flip(frame, 0).tostring()
-        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        ret, frame = self.cap.read()
+        ret, frame1 = self.cap.read()
+        ret, frame2 = self.cap.read()
+        diff = cv2.absdiff(frame1, frame2)
+        gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+        dilated = cv2.dilate(thresh, None, iterations=3)
+        contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if cv2.contourArea(contour) < 900:
+                continue
+            cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        buffer = cv2.flip(frame1, 0).tostring()
+        texture = Texture.create(size=(frame1.shape[1], frame1.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         self.image.texture = texture
 
