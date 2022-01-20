d394a06ba1575fcd289b4f4e756b006261119734
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-01-19 22:25:38 -0300

    update

--- Image-Handling/5MotionDetector.py
+++ Image-Handling/5MotionDetector.py
@@ -4,12 +4,12 @@ cap = cv2.VideoCapture(0)
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
-out = cv2.VideoWriter("output.avi", fourcc, 15.0, (1366, 768))
+out = cv2.VideoWriter("output.avi", fourcc, 15.0, (1280, 720))
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 while cap.isOpened():
-    image = cv2.resize(frame1, (1366, 768))
+    image = cv2.resize(frame1, (1280, 720))
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -25,7 +25,7 @@ while cap.isOpened():
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                     1, (0, 0, 255), 3)
-    image = cv2.resize(frame1, (1366, 768))
+    image = cv2.resize(frame1, (1280, 720))
     cv2.imshow("feed", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
