905d1292cc606054e2c328d9591bdaf94d2084ad
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-06-29 20:11:18 -0300

    interpolate

--- Science/Numpy/1Nbasics.py
+++ Science/Numpy/1Nbasics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import scipy.interpolate as interpolate
 
 """
 from random import randint
@@ -44,14 +45,27 @@ zeros = np.zeros(shape=(3, 3, 3, 3))  # 4 dims  -- ones 0 -> 1
 
 prices = np.full(100, fill_value=np.nan)
 prices[[0, 25, 60, -1]] = [80., 30., 75., 50.]
-plt.plot([80., 30., 75., 50.])
-# Linearly interpolate the missing values and add some noise.
-x = np.arange(len(prices))
+days = np.arange(len(prices))
 is_valid = ~np.isnan(prices)
-prices = np.interp(x=x, xp=x[is_valid], fp=prices[is_valid])  # val, val i
-print(prices)
-prices += np.random.randn(len(prices))
+prices = np.interp(x=days, xp=days[is_valid], fp=prices[is_valid])  # val, val i
+prices += np.random.randn(len(prices)) * 2
 
 fig, ax = plt.subplots()
 ax.plot(prices)
+ax.plot(prices, 'bo')
+# plt.show()
+
+
+y = np.random.random(20)
+x = np.arange(0, y.size)
+
+new_length = 80
+new_x = np.linspace(x.min(), x.max(), new_length)
+new_y = interpolate.interp1d(x, y, kind='linear')(new_x)
+
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(x, y, 'ro-')
+plt.subplot(2, 1, 2)
+plt.plot(new_x, new_y, 'bo-')
 plt.show()
