4d9e8d5ad24777b4873b760e3884d529ec159c8a
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-01 14:36:30 -0300

    cyber punk theme

--- Science/Pandas/3PlotingPandas.py
+++ Science/Pandas/3PlotingPandas.py
@@ -7,9 +7,28 @@ norte_shopping.dropna()
 norte_shopping = norte_shopping.groupby('Data').sum()
 print(norte_shopping)
 
-plt.plot(norte_shopping['Quantidade'] * 20, 'b--o', label='Produtos vendidos')
-plt.plot(norte_shopping['Valor Final'], 'r-o', label='Valor arrecadado')
-plt.plot((norte_shopping['Valor Final'] / norte_shopping['Quantidade']) * 20, linestyle='dotted', color='green', label='Vendas/faturamento')
+
+plt.style.use("seaborn-dark")
+
+for param in ['figure.facecolor', 'axes.facecolor', 'savefig.facecolor']:
+    plt.rcParams[param] = '#212946'  # bluish dark grey
+
+for param in ['text.color', 'axes.labelcolor', 'xtick.color', 'ytick.color']:
+    plt.rcParams[param] = '0.9'  # very light grey
+
+fig, ax = plt.subplots()
+ax.grid(color='#2A3459')  # bluish dark grey, but slightly lighter than background
+colors = [
+    '#08F7FE',  # teal/cyan
+    '#FE53BB',  # pink
+    '#F5D300',  # yellow
+    '#00ff41'   # matrix green
+]
+plt.xticks(rotation=30)
+
+plt.plot(norte_shopping['Quantidade'] * 20, '--.', label='Produtos vendidos', color=colors[3])
+plt.plot(norte_shopping['Valor Final'], '--.', label='Valor arrecadado', color=colors[0])
+plt.plot((norte_shopping['Valor Final'] / norte_shopping['Quantidade']) * 20, linestyle='dotted', color=colors[1], label='Vendas/faturamento', marker='.')
 plt.title('Norte Shopping - Vendas 01/01 - 07/02')
 plt.legend(loc="upper left")
 plt.show()
