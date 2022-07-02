af9cf2feead7622fc68bb4f3622374641982846f
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-07-02 00:16:01 -0300

    more of git commits

--- Science/Matplotlib/1MplBasics.py
+++ Science/Matplotlib/1MplBasics.py
@@ -1,2 +1,21 @@
 from matplotlib import pyplot as plt
+import numpy as np
+
+x = np.linspace(2, 8*np.pi, 400)
+fig, axs = plt.subplots(3)
+
+y = np.sin(x)
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y, color='#ff0000')
+
+plt.plot(x, y)
+
+y = np.cos(x)
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y, color='#ff0000')
+
+
+plt.show()
 
