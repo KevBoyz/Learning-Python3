30565bf5e390a615d5ceef470f0eb61363b1437c
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-12-01 23:56:45 -0300

    upgrades

--- Built-in/lambda.py
+++ Built-in/lambda.py
@@ -12,3 +12,8 @@ odds = filter(lambda x: x % 2 != 0, [1, 2, 3, 4, 5, 6])
 for n in odds:
     numbers.remove(n)
 print(numbers)
+
+
+from itertools import takewhile
+
+print(list(takewhile(lambda x: x <= 5, list(range(1, 11)))))
