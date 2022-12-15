5ee91c72a2f3f3a3baa1b1b3cef3f2512b59ee7c
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-12-15 16:29:39 -0300

    pizzaria completa

--- Built-in/Object Oriented Programing/Again/pizza_class.py
+++ Built-in/Object Oriented Programing/Again/pizza_class.py
@@ -38,5 +38,52 @@ class MeioAMeio(Pizza):
         return self.sabor1.ingredientes() + self.sabor2.ingredientes()
 
 
-ma = MeioAMeio(Mussarela, Calabreza)
-print(ma.ingredientes())
+
+class Pizzaria:
+    def __init__(self) -> None:
+        self._forno = Forno()  # Encapsing 
+        self._pizzaiolo = Pizzaolo()
+
+    def pedido(self, pizza):
+        pizza_pronta = self._pizzaiolo.preparar(pizza)
+        print('Aqui está sua Pizza:', pizza_pronta)
+
+    
+class Forno:
+    def __init__(self) -> None:
+        self.lenha = 0
+        self.pizzas = []
+
+    def ligar(self):
+        if self.lenha == None:
+            raise 'NãoHáLenha'
+        else:
+            self.lenha -= 1 # Assando
+
+    def desligar(self):
+        ...
+
+
+class Pizzaolo:
+    def __init__(self):
+        self.forno = Forno()
+
+    def preparar(self, pizza):
+        massa = []
+        for i in pizza.ingredientes():
+            massa.append(i)
+        return self.assar(massa)
+
+    def assar(self, massa):
+        # Coloca no forno
+        try:
+            self.forno.ligar()
+        except:
+            self.forno.lenha += 1
+            self.forno.ligar()
+        self.forno.desligar()
+        return self.tirar(massa)
+
+    def tirar(self, pizza_pronta):
+        return pizza_pronta
+            
