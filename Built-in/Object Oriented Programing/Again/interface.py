14ab8111892b434a5478260cd764f394df864cee
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-12-18 17:04:21 -0300

    update

--- Built-in/Object Oriented Programing/Again/interface.py
+++ Built-in/Object Oriented Programing/Again/interface.py
@@ -1,14 +1,36 @@
-from _collections_abc import Container, Sized as abc_container 
+from _collections_abc import Container as abc_container, Sized, Collection
+from typing import Iterator
 
 
-class Container:
+class Container(abc_container, Sized):  # Explicit implementation
     def __init__(self, c) -> None:
         self.c = c
     def __contains__(self, v):  # Container protocol
-        return v in list(self.c)
+        return v in list(self.c)  # This make this class a container
     def __len__(self):  # Sized protocol
-        return len(self.c)
+        return len(self.c)  # This make this class a sizeble
 
     
-c = Container([1,2,3])
-print(issubclass(Container, abc_container))
+# c = Container([1,2,3])
+# print(issubclass(Container, abc_container))
+
+
+class Coleção(Collection):  # Try remove one of this methods
+    def __init__(self, *c) -> None:
+        super().__init__()
+        self.c = c
+
+    def __contains__(self, __x: object) -> bool:
+        return super().__contains__(__x)
+    
+    def __len__(self) -> int:
+        return super(self.c).__len__()
+
+    def __iter__(self) -> Iterator:
+        return super().__iter__()
+
+"""    def __getitem__(self, i):
+        return super().__getitem__(i)"""
+
+
+c = Coleção(1,2,3,4,5)
