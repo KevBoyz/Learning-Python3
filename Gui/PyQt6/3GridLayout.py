222db796e07f97d3b3d7a273b23621d22b1eb800
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-12-26 16:11:56 -0300

    new arg

--- Gui/PyQt6/3GridLayout.py
+++ Gui/PyQt6/3GridLayout.py
@@ -1,12 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
+from PyQt6.QtCore import Qt
 
 app = QApplication([])
 
 layout = QGridLayout()
-layout.addWidget(QPushButton("Button"), 0, 0)
-layout.addWidget(QPushButton("Button"), 0, 1)
-layout.addWidget(QPushButton("Button"), 0, 2)
+layout.addWidget(QPushButton("Button"), 0, 0, alignment=Qt.AlignmentFlag.AlignRight)
+# layout.addWidget(QPushButton("Button"), 0, 1)
+layout.addWidget(QPushButton("Button"), 0, 2, alignment=Qt.AlignmentFlag.AlignLeft)
 layout.addWidget(QPushButton("Button"), 1, 0, 1, 3)
 layout.addWidget(QPushButton("Button"), 2, 0, 1, 1)
 layout.addWidget(QPushButton("Button"), 2, 1, 1, 2)
@@ -18,4 +19,3 @@ window.setLayout(layout)
 
 window.show()
 sys.exit(app.exec())
-
