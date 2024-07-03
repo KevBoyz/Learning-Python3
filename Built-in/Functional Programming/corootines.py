bf0c54cc0b8ed011afc7fa935c31050ada369c99
KevBoyz <Kevinho_gameplays@hotmail.com>
2024-07-03 14:18:06 -0300

    abcd

--- Built-in/Functional Programming/corootines.py
+++ Built-in/Functional Programming/corootines.py
@@ -24,14 +24,35 @@ s.add_coro(range(0, 10))
 s.add_coro(range(11, 15))
 #s.run()
 
+# 02
+
 
 def coro():
-    print('Mim de')
-    val = yield
-    print(val)
+    while True:
+        val = yield
+        print(val)
 
 
-c = coro()
-next(c)
+"""c = coro()
+next(c)  # init
 c.send('Ahh Zé da manga')
+c.close()
+"""
+
+def runtime_mean():
+    mean = None
+    n = 0
+    total = 0
+    while True:
+        inp = yield mean
+        n += 1
+        total += inp
+        mean = total / n
+
+
+m = runtime_mean()
+next(m)
+print(m.send(10))
+print(m.send(15))
+
 
